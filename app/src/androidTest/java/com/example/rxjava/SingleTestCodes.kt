4e0393b82dd27ea99939e40b21cc77c817668ada
hakzzang <mbh930312@naver.com>
2022-03-10 00:58:03 +0900

    [Single] SingleTestCodes.kt 생성

---
+++ app/src/androidTest/java/com/example/rxjava/SingleTestCodes.kt
@@ -0,0 +1,11 @@
+package com.example.rxjava
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.reactivex.rxjava3.core.Single
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SingleTestCodes {
+
+}
