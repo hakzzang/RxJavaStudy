ae60810a042e2543e35d66a259403b4f4d89d385
hakzzang <mbh930312@naver.com>
2022-03-27 18:54:15 +0900

    [Observable][combining] zip연산자_테스트_코드_추가

--- app/src/androidTest/java/com/example/rxjava/Page151CombiningOperatorsTest.kt
+++ app/src/androidTest/java/com/example/rxjava/Page151CombiningOperatorsTest.kt
@@ -44,4 +44,42 @@ class Page151CombiningOperatorsTest {
         Thread.sleep(5000L)
         test.assertValues("1A", "2A", "2B", "2C", "3C", "3D", "4D", "5D")
     }
+
+    @Test
+    fun `zip연산자_테스트`() {
+        val src1 = Observable.create<Int> { emitter ->
+            Thread {
+                for (number in 1..5) {
+                    emitter.onNext(number)
+                    try {
+                        Thread.sleep(1000L)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }.start()
+        }
+
+        val src2 = Observable.create<String> { emitter ->
+            Thread {
+                try {
+                    Thread.sleep(500L)
+                    emitter.onNext("A")
+                    Thread.sleep(700L)
+                    emitter.onNext("B")
+                    Thread.sleep(100L)
+                    emitter.onNext("C")
+                    Thread.sleep(700L)
+                    emitter.onNext("D")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+
+        val result = Observable.zip(src1, src2) { number, word -> number.toString() + word }
+        val test = result.test()
+        Thread.sleep(5000L)
+        test.assertValues("1A", "2B", "3C", "4D")
+    }
 }
