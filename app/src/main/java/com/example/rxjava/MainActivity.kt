a4e7f7886d4a1e22bd0a8cd4bc19794cd8e13b46
hakzzang <mbh930312@naver.com>
2022-03-28 00:27:38 +0900

    [Observable][Flowable] flowable 테스트 코드 추가

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rxjava
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rxjava.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
@@ -13,10 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(ActivityMainBinding.inflate(layoutInflater).root)
 
-        main쓰레드_스케듈러_테스트코드()
-        subscribeOn_io쓰레드_스케듈러_테스트코드()
-        computation쓰레드_스케듈러_테스트코드()
-        subscribeOn_io쓰레드_174_스케듈러_테스트코드()
+//        main쓰레드_스케듈러_테스트코드()
+//        subscribeOn_io쓰레드_스케듈러_테스트코드()
+//        computation쓰레드_스케듈러_테스트코드()
+//        subscribeOn_io쓰레드_174_스케듈러_테스트코드()
+//        Observable_발생과소비과동일한쓰레드에서발생시키기()
+//        Observable_발생과소비과다른쓰레드에서발생시키기()
+//        Flowable_interval을_onBackpressureBuffer를_통해서발생시키기()
+        Flowable_interval을_onBackpressureLatest를_통해서발생시키기()
     }
 
     //page171
@@ -94,5 +99,85 @@ class MainActivity : AppCompatActivity() {
             val subscribedThreadName = Thread.currentThread().name
             println("#subscribeOn_io쓰레드_174-subscribe : $subscribedThreadName number:$time")
         }
+    }
+
+    fun `Observable_발생과소비과동일한쓰레드에서발생시키기`() {
+        Observable.range(1, Int.MAX_VALUE)
+            .map { item ->
+                println("#아이템발행:$item")
+                return@map item
+            }.subscribe { item ->
+                Thread.sleep(100)
+                println("#아이템소비:$item")
+            }
+        Thread.sleep(30 * 1000)
+    }
+
+    fun `Observable_발생과소비과다른쓰레드에서발생시키기`() {
+        Observable.range(1, Int.MAX_VALUE)
+            .map { item ->
+                println("#아이템발행:$item")
+                return@map item
+            }.observeOn(Schedulers.io()).subscribe { item ->
+                Thread.sleep(100)
+                println("#아이템소비:$item")
+            }
+        Thread.sleep(30 * 1000)
+    }
+
+    fun `Flowable_발생과소비과다른쓰레드에서발생시키기`() {
+        Flowable.range(1, Int.MAX_VALUE)
+            .map { item ->
+                println("#아이템발행:$item")
+                return@map item
+            }.observeOn(Schedulers.io()).subscribe { item ->
+                Thread.sleep(100)
+                println("#아이템소비:$item")
+            }
+        Thread.sleep(10 * 1000)
+    }
+
+    fun `Flowable_interval을_통해서발생시키기`() {
+        // 1. error 발생 확인 - OnErrorNotImplementedException
+        // 2. flowable의 buffer-size 는 128, 구현부에 있음
+        // 3. 시간에 의해서 갭이 발생되게 되면 error 발생
+        Flowable.interval(100, TimeUnit.MILLISECONDS)
+            .map { item ->
+                println("#아이템발행:$item")
+                return@map item
+            }.observeOn(Schedulers.io()).subscribe { item ->
+                Thread.sleep(1000)
+                println("#아이템소비:$item")
+            }
+
+        Thread.sleep(10 * 1000)
+    }
+
+    fun `Flowable_interval을_onBackpressureBuffer를_통해서발생시키기`() {
+        //bufferSize- 128, onBackPressureBuffer에서 확인
+        //오래된 아이템을 buffer에 유지해놓기 때문에 아이템값을 유지
+        Flowable.interval(10, TimeUnit.MILLISECONDS)
+            .onBackpressureBuffer()
+            .map { item ->
+                println("#아이템발행:$item")
+                return@map item
+            }.observeOn(Schedulers.io()).subscribe { item ->
+                Thread.sleep(100)
+                println("#아이템소비:$item")
+            }
+    }
+
+    fun `Flowable_interval을_onBackpressureLatest를_통해서발생시키기`() {
+        //bufferSize- 128, onBackPressureBuffer에서 확인
+        //최신 아이템을 buffer에 유지해놓기 때문에 아이템소비가 빠르게 오름
+        Flowable.interval(10, TimeUnit.MILLISECONDS)
+            .onBackpressureLatest()
+            .map { item ->
+                println("#아이템발행:$item")
+                return@map item
+            }.observeOn(Schedulers.io()).subscribe { item ->
+                Thread.sleep(100)
+                println("#아이템소비:$item")
+            }
     }
 }
