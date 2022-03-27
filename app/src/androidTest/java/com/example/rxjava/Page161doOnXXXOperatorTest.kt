170fbddda4027975b657e0584e5193a506eed8ff
hakzzang <mbh930312@naver.com>
2022-03-27 19:31:04 +0900

    [Observable][doOn] doOnEach 연산자 확인 로직 추가

---
+++ app/src/androidTest/java/com/example/rxjava/Page161doOnXXXOperatorTest.kt
@@ -0,0 +1,26 @@
+package com.example.rxjava
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.reactivex.rxjava3.core.Observable
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class Page161doOnXXXOperatorTest {
+
+    @Test
+    fun `doOnEach연산자`() {
+        Observable.just(1, 2, 3)
+            .doOnEach { notification ->
+                println("value:${notification.value}")
+                println("isOnNext:${notification.isOnNext}")
+                println("isOnComplete:${notification.isOnComplete}")
+                println("isOnError:${notification.isOnError}")
+                println("error:${notification.error}")
+
+                if (notification.error != null) {
+                    notification.error.printStackTrace()
+                }
+            }.subscribe { println("subscribed = $it") }
+    }
+}
