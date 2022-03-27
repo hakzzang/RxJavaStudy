128aadbc6b1eec0ed9ea75f0f6ad47d647c30ea0
hakzzang <mbh930312@naver.com>
2022-03-27 22:18:15 +0900

    [Observable][Scheduler] main스레드 스케쥴러 테스트 코드 추가

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -10,49 +10,33 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.rxjava.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Observable
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(ActivityMainBinding.inflate(layoutInflater).root)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        main쓰레드_스케듈러_테스트코드()
+    }
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    //page171
+    private fun `main쓰레드_스케듈러_테스트코드`() {
+        println("##########main쓰레드_스케듈러_테스트코드-start########")
+        val src = Observable.create<Int> { emitter ->
+            for (number in 0 until 3) {
+                val threadName = Thread.currentThread().name
+                println("#쓰레드-create : $threadName number:$number")
+                emitter.onNext(number)
+                Thread.sleep(100L)
+            }
+            emitter.onComplete()
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        src.subscribe { number ->
+            val subscribedThreadName = Thread.currentThread().name
+            println("#쓰레드-subscribe : $subscribedThreadName number:$number")
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        println("##########main쓰레드_스케듈러_테스트코드-end########")
     }
 }
